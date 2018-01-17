23382c4f050fdf1947cded46a46a0920ce20c63a
Jakob Holderbaum <jakob@phrased.org>
2018-01-17 21:11:18 +0100

    phrased version v0.1.4

--- phrased.rb
+++ phrased.rb
@@ -1,9 +1,9 @@
 class Phrased < Formula
   desc "Generate awesome passphrases from a set of dictionaries"
   homepage "https://phrased.org/"
-  url "https://github.com/phrased-org/phrased/releases/download/v0.1.3/phrased_0.1.3_OSX-64bit.tar.gz"
-  version "0.1.3"
-  sha256 "5c1a138dd15c8d140a7756167097e67a5372114d61cdb428ce0763a7368a2870"
+  url "https://github.com/phrased-org/phrased/releases/download/v0.1.4/phrased_0.1.4_OSX-64bit.tar.gz"
+  version "0.1.4"
+  sha256 "36aa95ecd81d492f956634094798ee9d962da5f4ebc044fad4eb884b2779306f"
 
   def install
     bin.install "phrased"
@@ -11,6 +11,5 @@ class Phrased < Formula
 
   test do
     system "#{bin}/phrased"
-    ...
   end
 end
