b0a7cd4b53cfd8a6db810ef0704250e19e9bd145
Jakob Holderbaum <jakob@phrased.org>
2018-03-14 22:47:45 +0100

    Brew formula update for phrased version v0.2

--- phrased.rb
+++ phrased.rb
@@ -1,9 +1,9 @@
 class Phrased < Formula
   desc "Generate awesome passphrases from a set of dictionaries"
   homepage "https://phrased.org/"
-  url "https://github.com/phrased-org/phrased/releases/download/v0.1.4/phrased_0.1.4_OSX-64bit.tar.gz"
-  version "0.1.4"
-  sha256 "36aa95ecd81d492f956634094798ee9d962da5f4ebc044fad4eb884b2779306f"
+  url "https://github.com/phrased-org/phrased/releases/download/v0.2/phrased_0.2_OSX-64bit.tar.gz"
+  version "0.2"
+  sha256 "164b3099de9c708b257b2f315f0dd03d4a54b7a8ad2d9dd523114b740222a568"
 
   def install
     bin.install "phrased"
